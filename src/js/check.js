af069d736f6d06c3bdb5d667869a21530467d98a
Wouter van der Heijde <wouterh@MacBook-Pro-van-Wouter-3.local>
2020-03-05 13:21:39 +0100

    :lipstick:adding styles

--- src/js/check.js
+++ src/js/check.js
@@ -2,15 +2,17 @@ import {
     renderData
 } from "./render.js";
 
+const body = document.querySelector("body")
 
 function checkAge(value, genreValue , section) {
  
     if (value <= 12 ) {
         renderData(JSON.parse(localStorage.getItem(`dataYouth${genreValue}`)), section)
+        body.classList.add("kind")
 
     } else {
         renderData(JSON.parse(localStorage.getItem(`dataOld${genreValue}`)), section)
-
+        body.classList.remove("kind")
     }
 }
 
